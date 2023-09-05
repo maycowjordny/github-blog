3a4f4e6f31a70d6fefae8f79be8a44939b80f749
Maycow jordny <maycowjordny2015@gmail.com>
2023-09-05 10:15:14 -0300

    criando client e criando função de pesquisar issues

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,26 +4,26 @@ import { CardWrapper, HomeContainer, SearchWrapper } from "./styles";
 import { useState, useEffect } from "react"
 import { IssueProps } from "../../interfaces";
 import axios from "axios"
-import { useParams } from "react-router-dom";
 
 export function Home() {
 
-  const { number } = useParams()
   const [issues, setIssue] = useState<IssueProps[]>([])
   const [search, setSearch] = useState("")
-  useEffect(() => {
-    async function FetchIssues() {
-      const response = await axios('https://api.github.com/search/issues?q=repo:maycowjordny/github-blog')
-      setIssue(response.data.items)
-    }
-    FetchIssues()
-  }, [])
 
+  /*   useEffect(() => {
+      async function FetchIssues() {
+        const response = await axios('https://api.github.com/search/issues?q=repo:maycowjordny/github-blog')
+        setIssue(response.data.items)
+      }
+      FetchIssues()
+    }, [])
+   */
   useEffect(() => {
     async function SearchIssues() {
-      const response = await axios(`https://api.github.com/search/issues?q=${search}repo:maycowjordny/github-blog`)
-      setIssue(response.data.items);
-
+      const response = await axios(`https://api.github.com/search/issues?q=${search}%20repo:maycowjordny/github-blog`)
+      if (response) {
+        setIssue(response.data.items);
+      }
     }
     SearchIssues()
   }, [search])
